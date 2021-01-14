80410ad02b3536ea4fdc9a7701fbfc219a00bd46
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-01-15 00:07:29 +0300

    add UC 4

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const input = document.getElementById("input-destination");
 
 /* Array of Objects */
 
-let dropdownList = [
+const dropdownList = [
 	{
 		label: "Bawcomville",
 		id: 0,
@@ -36,12 +36,14 @@ let dropdownList = [
 
 /* UserCase 3: Choose from Dropdown List */
 
+let selectedElement = false;
+
 const selectDropdownElementHandler = (event) => {
 	event.target.classList.toggle("dropdown-content_element");
 	input.value = event.target.textContent;
+	selectedElement = true;
 	button.classList.remove("dropdown-btn-open");
 	button.classList.add("dropdown-btn-close");
-
 };
 
 /* Change List Element background, when you point mouse at it */
@@ -52,12 +54,13 @@ const highlightDropdownElementHandler = (event) => {
 
 /* Add List Elements in Markup */
 
-let dropdownElementHandler = () => {
+const dropdownElementHandler = () => {
 	let fragment = document.createDocumentFragment();
 
 	dropdownList.forEach(function (dropDownItem) {
 		let listElement = document.createElement("li");
 		listElement.textContent = dropDownItem.label;
+		selectedElement = false;
 		listElement.addEventListener("click", selectDropdownElementHandler);
 		listElement.addEventListener("mouseover", highlightDropdownElementHandler);
 
@@ -69,38 +72,31 @@ let dropdownElementHandler = () => {
 
 window.addEventListener("load", dropdownElementHandler);
 
-/* Change arrow icon on close / open Dropdown*/
-
-let changeDropdownIconHandler = () => {
-	if (button.classList.contains("dropdown-btn-open")) {
-		button.classList.remove("dropdown-btn-open");
-		button.classList.add("dropdown-btn-close");
-	} else if (button.classList.contains("dropdown-btn-close")) {
-		button.classList.remove("dropdown-btn-close");
-		button.classList.add("dropdown-btn-open");
-	}
-};
-
 /* UserCase 1: Open List */
 
-let openDropdownHandler = () => {
+const openDropdownHandler = () => {
 	getDropdownContent().classList.toggle("show-dropdown-content");
+	if (button.classList.contains("dropdown-btn-close")) {
+		button.classList.remove("dropdown-btn-close");
+		button.classList.add("dropdown-btn-open");
+	} else if (button.classList.contains("dropdown-btn-open")) {
+		button.classList.remove("dropdown-btn-open");
+		button.classList.add("dropdown-btn-close");
+	}
 	input.focus();
 	input.value = "";
 	searchDropdownElementHandler();
 };
 
 button.addEventListener("click", openDropdownHandler);
-button.addEventListener("click", changeDropdownIconHandler);
-
 input.addEventListener("click", openDropdownHandler);
-input.addEventListener("click", changeDropdownIconHandler);
+
 
 /* UserCase 2: Filter List Elements */
 
-let searchDropdownElementHandler = () => {
+const searchDropdownElementHandler = () => {
 	input.focus();
-	getDropdownContent().classList.add("show-dropdown-content");
+	// getDropdownContent().classList.add("show-dropdown-content");
 	const inputValue = input.value.toLowerCase();
 
 	let matchingElements = dropdownList.filter(function (dropDownItem) {
@@ -113,36 +109,45 @@ let searchDropdownElementHandler = () => {
 	let list = document.createElement("ul");
 	list.setAttribute("id", "list");
 
-	let fragment = document.createDocumentFragment();
-
 	for (let i = 0; i < matchingElements.length; i++) {
 		let listElement = document.createElement("li");
 		listElement.textContent = matchingElements[i].label;
+		selectedElement = false;
 		listElement.addEventListener("click", selectDropdownElementHandler);
 		listElement.addEventListener("mouseover", highlightDropdownElementHandler);
-		fragment.appendChild(listElement);
+		list.appendChild(listElement);
 	}
 
 	list.classList.add("dropdown-content", "show-dropdown-content");
-	list.appendChild(fragment);
 
 	getDropdownContent().parentNode.replaceChild(list, getDropdownContent());
 };
 
 input.addEventListener("input", searchDropdownElementHandler);
 
-/* UNDONE UserCase 4: Close dropdown without choosing List Element*/
+/* UserCase 4: Close dropdown without choosing List Element*/
 
-let closeDropdownHandler = (event) => {
+const closeDropdown = () => {
+	getDropdownContent().classList.remove("show-dropdown-content");
+	button.classList.remove("dropdown-btn-open");
+	button.classList.add("dropdown-btn-close");
+
+	if (!selectedElement) {
+		input.value="";
+	}
+};
+
+const closeDropdownHandler = (event) => {
 	if (
 		!input.contains(event.target) &&
     !button.contains(event.target) &&
     !getDropdownContent().contains(event.target)
 	) {
-		getDropdownContent().classList.remove("show-dropdown-content");
-		button.classList.remove("dropdown-btn-open");
-		button.classList.add("dropdown-btn-close");
+		closeDropdown();
 	}
 };
 
 document.addEventListener("click", closeDropdownHandler);
+
+window.addEventListener("scroll", closeDropdown);
+window.addEventListener("resize", closeDropdown);
